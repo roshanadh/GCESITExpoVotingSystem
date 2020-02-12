c8094d0761fa47b10ff78e6a4bb50c4f8a76104c
Roshan Adhikari <nahsorad@gmail.com>
2020-02-12 22:09:58 +0545

    Connect to remotemysql database

--- src/np/edu/gces/itexpo/VoterRegistration.java
+++ src/np/edu/gces/itexpo/VoterRegistration.java
@@ -2,8 +2,16 @@ package np.edu.gces.itexpo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
 
 public class VoterRegistration extends JFrame {
+	private String databaseURL;
+	private String databaseID;
+	private String databaseName;
+	private String databasePassword;
+
+	private Connection connection;
+
 	private JLabel nameLabel, phoneLabel, barcodeLabel;
 	private JTextField nameField, phoneField, barcodeField;
 	private JButton registerButton, showVotersButton;
@@ -33,7 +41,7 @@ public class VoterRegistration extends JFrame {
 
 		this.add(registerButton);
 		this.add(showVotersButton);
-		
+
 		this.setLayout(new FlowLayout());
 		this.setTitle("8th GCES IT Expo Voter Registration");
 		this.setSize(500, 500);
@@ -41,6 +49,30 @@ public class VoterRegistration extends JFrame {
 		this.setAlwaysOnTop(true);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+
+		try {
+			PreparedStatement statement;
+			
+//			Load database credentials from custom EnvironmentVariable class
+			databaseURL = EnvironmentVariable.load("databaseURL");
+			databaseID = EnvironmentVariable.load("databaseID");
+			databaseName = EnvironmentVariable.load("databaseName");
+			databasePassword = EnvironmentVariable.load("databasePassword");
+
+			System.out.println("Connecting to remote database...");
+			connection = DriverManager.getConnection(databaseURL, databaseID, databasePassword);
+			System.out.println("Connection to remote database established.");
+			statement = connection.prepareStatement("USE " + databaseName);
+			statement.executeUpdate();
+			statement = connection.prepareStatement("INSERT INTO voters (name, phone, barcode, vote) VALUES ('ramesh', '9800000000', '12345678912', 1)");
+			statement.executeUpdate();
+		} catch(SQLException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Connection to database couldn't be established!", "MySQL Error", JOptionPane.ERROR_MESSAGE);
+		} catch(Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Error while loading environment variable: " + e.getMessage(), "Environment Variable Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 	public static void main(String[] args) {
 		new VoterRegistration();
