8fe82501ee533962cd9d8523bda602bded20af4a
Roshan Adhikari <nahsorad@gmail.com>
2020-02-13 22:12:11 +0545

    Add busy waiting connection checker

---
+++ src/np/edu/gces/itexpo/InternetChecker.java
@@ -0,0 +1,15 @@
+package np.edu.gces.itexpo;
+
+import java.io.*;
+import java.net.*;
+
+public class InternetChecker {
+	public static boolean isAvailable() {
+		try {
+			return InetAddress.getByName("google.com").isReachable(200) ? true : false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
